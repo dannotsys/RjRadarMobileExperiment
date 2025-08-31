b9f26c09c11aed841146f20a894097014a8a15bc
Danilo Lima <danilo.notsys@live.com>
2025-08-31 10:07:09 -0300

    Rollback

--- Radar/RadarCustom_files/mapaRadar.js
+++ Radar/RadarCustom_files/mapaRadar.js
@@ -1,6 +1,7 @@
 $(document).ready(function () {
     validarElementsDarkMode();
-
+    //
+    //var layerOSM = L.tileLayer('https://mt.google.com/vt/lyrs=r&f=q&x={x}&y={y}&z={z}&s=Gal&apistyle=s.t%3A3|s.e%3Ag|p.v%3Aoff,s.t%3A3|s.e%3Al|p.v%3Aoff,s.t%3A2|s.e%3Al|p.v%3Aoff', {
     var layerOSM = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png?', {
         attribution: '',
         minZoom: 3,
