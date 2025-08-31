5b8cda8ffbff7283874bab0b4245fadea3951730
Danilo Lima <danilo.notsys@gmail.com>
2025-08-30 22:12:13 -0300

    Update mapaRadar.js

--- Radar/RadarCustom_files/mapaRadar.js
+++ Radar/RadarCustom_files/mapaRadar.js
@@ -1,7 +1,7 @@
 $(document).ready(function () {
     validarElementsDarkMode();
 
-    var layerOSM = LtileLayerr('https://mt.google.com/vt/lyrs=m&x={x}&y={y}&z={z}?', {
+    var layerOSM = LtileLayerr('https://mt.google.com/vt/lyrs=m,transit&x={x}&y={y}&z={z}', {
         attribution: '',
         minZoom: 3,
         maxZoom: 16,
